fd4e1a7c7c2bb0635bc04623ac4f194b505ca5e7
Dwynr <dwynr@outlook.de>
2026-03-04 22:26:33 +0100

    perf(filen-mobile): convert extension constants from Array to Set
    All EXPO_*_EXTENSIONS constants are now Set<string> instead of string[],
    giving O(1) has() lookups instead of O(n) includes() scans at every call
    site (cameraUpload compress, getPreviewType).

--- packages/filen-mobile/src/constants.ts
+++ packages/filen-mobile/src/constants.ts
@@ -15,32 +15,32 @@ export const NETINFO_CONFIG: NetInfoConfiguration = {
 	reachabilityMethod: "HEAD"
 }
 
-export const EXPO_IMAGE_MANIPULATOR_SUPPORTED_EXTENSIONS = Platform.select({
+export const EXPO_IMAGE_MANIPULATOR_SUPPORTED_EXTENSIONS = new Set<string>(Platform.select({
 	ios: [".jpg", ".jpeg", ".png", ".gif", ".bmp", ".tiff", ".heic", ".heif", ".webp"],
 	android: [".jpg", ".jpeg", ".png", ".gif", ".bmp", ".webp", ".heic", ".heif"],
 	default: [".jpg", ".jpeg", ".png", ".gif", ".bmp"]
-}) as string[]
+}) as string[])
 
-export const EXPO_VIDEO_SUPPORTED_EXTENSIONS = Platform.select({
+export const EXPO_VIDEO_SUPPORTED_EXTENSIONS = new Set<string>(Platform.select({
 	ios: [".mp4", ".mov", ".m4v", ".3gp"],
 	android: [".mp4", ".webm", ".3gp", ".mkv"],
 	default: [".mp4", ".webm"]
-}) as string[]
+}) as string[])
 
-export const EXPO_AUDIO_SUPPORTED_EXTENSIONS = Platform.select({
+export const EXPO_AUDIO_SUPPORTED_EXTENSIONS = new Set<string>(Platform.select({
 	ios: [".mp3", ".m4a", ".aac", ".wav", ".aiff", ".caf", ".flac", ".alac"],
 	android: [".mp3", ".m4a", ".aac", ".wav", ".ogg", ".3gp", ".flac"],
 	default: [".mp3", ".m4a", ".aac", ".wav", ".ogg"]
-}) as string[]
+}) as string[])
 
-export const EXPO_VIDEO_THUMBNAILS_SUPPORTED_EXTENSIONS = Platform.select({
+export const EXPO_VIDEO_THUMBNAILS_SUPPORTED_EXTENSIONS = new Set<string>(Platform.select({
 	ios: [".mp4", ".mov", ".m4v", ".3gp"],
 	android: [".mp4", ".webm", ".3gp", ".mkv"],
 	default: [".mp4", ".webm"]
-}) as string[]
+}) as string[])
 
-export const EXPO_IMAGE_SUPPORTED_EXTENSIONS = Platform.select({
+export const EXPO_IMAGE_SUPPORTED_EXTENSIONS = new Set<string>(Platform.select({
 	ios: [".jpg", ".jpeg", ".png", ".gif", ".webp", ".avif", ".heic", ".svg", ".ico", ".icns"],
 	android: [".jpg", ".jpeg", ".png", ".gif", ".webp", ".avif", ".heic", ".svg", ".ico", ".icns"],
 	default: [".jpg", ".jpeg", ".png", ".gif", ".webp", ".avif", ".svg", ".ico", ".icns"]
-}) as string[]
+}) as string[])
