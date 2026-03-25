783a3fb96ef6f44d5b654aef6c2549d53cdf0b6f
Dwynr <dwynr@outlook.de>
2026-03-25 22:43:39 +0100

    perf(filen-mobile): strip console calls from production builds
    Add babel-plugin-transform-remove-console for production. Eliminates
    all console.log/warn/info/debug calls at compile time, avoiding
    argument evaluation overhead in release builds.

--- packages/filen-mobile/babel.config.js
+++ packages/filen-mobile/babel.config.js
@@ -3,6 +3,9 @@ module.exports = function (api) {
 
 	return {
 		presets: ["babel-preset-expo"],
-		plugins: ["react-native-worklets/plugin"]
+		plugins: [
+			"react-native-worklets/plugin",
+			...(process.env.NODE_ENV === "production" ? ["transform-remove-console"] : [])
+		]
 	}
 }
