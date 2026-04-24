ea7c569a00f2c825daae9de9ca3e787475009ad4
Dwynr <dwynr@outlook.de>
2026-04-24 16:51:05 +0200

    fix(ci): deps installing

--- packages/filen-mobile/scripts/postinstall.sh
+++ packages/filen-mobile/scripts/postinstall.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-# Skip patch-package in CI environments (EAS Build, GitHub Actions, etc.)
+# Skip patch-package in CI environments (EAS Build, GitHub Actions, etc.). Needs to be run directly in the CI yaml.
 if [ -n "$CI" ] || [ -n "$EAS_BUILD" ]; then
 	exit 0
 fi
