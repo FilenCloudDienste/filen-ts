6a53f2f480c284e6a1c3ee6b11bcf12015fad31f
Dwynr <dwynr@outlook.de>
2026-04-24 14:50:09 +0200

    feat(filen-mobile): better sdk error logging

--- packages/filen-mobile/src/global.ts
+++ packages/filen-mobile/src/global.ts
@@ -1,6 +1,7 @@
+import "@/lib/polyfills/DOMException"
 import "@/lib/polyfills/buffer"
 import "@/lib/polyfills/crypto"
-import "@/lib/polyfills/DOMException"
+import "@/lib/polyfills/console"
 
 import NetInfo from "@react-native-community/netinfo"
 import { NETINFO_CONFIG } from "@/constants"
