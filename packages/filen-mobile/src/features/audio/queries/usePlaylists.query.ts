96cb633b0d28c33b3a6d24147c7d94d5c78b3068
Dwynr <dwynr@outlook.de>
2026-06-08 01:54:33 +0200

    fix(filen-mobile): cache drive item from playlist tracks

--- packages/filen-mobile/src/features/audio/queries/usePlaylists.query.ts
+++ packages/filen-mobile/src/features/audio/queries/usePlaylists.query.ts
@@ -1,11 +1,22 @@
 import { useQuery, type UseQueryOptions, type UseQueryResult } from "@tanstack/react-query"
 import { DEFAULT_QUERY_OPTIONS, queryUpdater } from "@/queries/client"
 import audio from "@/features/audio/audio"
+import cache from "@/lib/cache"
 
 export const BASE_QUERY_KEY = "usePlaylistsQuery"
 
 export async function fetchData(params?: { signal?: AbortSignal }) {
-	return await audio.getPlaylists(params?.signal)
+	const playlists = await audio.getPlaylists(params?.signal)
+
+	for (const playlist of playlists) {
+		for (const { item } of playlist.files) {
+			// We need to cache it here for the audioMetadata query to work later, since it relies on the cache
+
+			cache.uuidToAnyDriveItem.set(item.data.uuid, item)
+		}
+	}
+
+	return playlists
 }
 
 export function usePlaylistsQuery(
